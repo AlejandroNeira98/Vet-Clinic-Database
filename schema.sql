cc0cb703a659bcb2ab2ff2c3f03bb91cc18f3b64
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-05-04 10:56:50 -0500

    Add column

--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals( id INT, name TEXT, date_of_birth DATE, escape_attempts INT, neutered BOOL, weight_kg FLOAT)
+
+/*  ADD COLUMN */
+ALTER TABLE animals ADD species varchar(50);
