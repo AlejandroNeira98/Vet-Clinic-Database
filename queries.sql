9ec7e6c9fee1cf195635ddc1575d3ab766c9e091
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-05-04 11:41:55 -0500

    Add queries

--- queries.sql
+++ queries.sql
@@ -16,3 +16,16 @@ SELECT name FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 /*  - Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
 SELECT name FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+/*   - How many animals are there? */
+SELECT COUNT(*) FROM animals;
+/*   - How many animals have never tried to escape? */
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+/*   - What is the average weight of animals? */
+SELECT AVG(weight_kg) FROM animals;
+/*   - Who escapes the most, neutered or not neutered animals? */
+SELECT SUM(escape_attempts) FROM animals GROUP BY neutered;
+SELECT AVG(escape_attempts) FROM animals GROUP BY neutered;
+/*   - What is the minimum and maximum weight of each type of animal? */
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+/*   - What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31' GROUP BY species;
