103c9a95d14b1af2bea3e5d54d4c214ab9f8b569
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-05-05 18:10:24 -0500

    Modify animals to insert owner's id

--- schema.sql
+++ schema.sql
@@ -9,7 +9,6 @@ ALTER TABLE animals ADD species varchar(50);
 CREATE TABLE owners( id SERIAL PRIMARY KEY, full_name TEXT, age INT );
 
 /* Create a table named species */
-
 CREATE TABLE species( id SERIAL PRIMARY KEY, name TEXT);
 
 /* Modify animals table:
@@ -17,12 +16,15 @@ Make sure that id is set as autoincremented PRIMARY KEY */
 ALTER TABLE animals 
   DROP COLUMN id,
   ADD COLUMN id SERIAL PRIMARY KEY;
+
 /* Remove column species */
 ALTER TABLE animals 
   DROP COLUMN species;
+
 /* Add column species_id which is a foreign key referencing species table */
 ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
 /* Add column owner_id which is a foreign key referencing the owners table */
 ALTER TABLE animals 
   ADD COLUMN owner_id INT,
